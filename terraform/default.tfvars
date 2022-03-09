089db409541ee55aabeca349908b170c3c1a62fb
Youssef Riahi <youssef.riahi@mass.gov>
2022-03-09 11:51:04 -0500

    terraform fmt

--- terraform/default.tfvars
+++ terraform/default.tfvars
@@ -1,3 +1,3 @@
-region = "us-east-1"
-log_retention = 3
+region         = "us-east-1"
+log_retention  = 3
 lambda_timeout = 900
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,12 +3,12 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = "~> 2.2.0"
-      
+
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.4.0"
     }
   }
